434c6e7ac77349ef3b6a576fb2edfac915372670
Udita Bose <udita.bose@mongodb.com>
2023-03-06 10:49:13 -0500

    skunk23: initialize repo

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# create the virtual env
+# python3 -m venv env
+
+# activate virtual env
+source env/bin/activate
+
+# install libraries
+env/bin/python3 -m pip install -r requirements.txt
